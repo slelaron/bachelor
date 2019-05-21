ca894583d3136229c4d3cbb8e501286ada20eadb
slelaron <slelaron@gmail.com>
2019-05-21 08:54:48 +0300

    redundant motions in r.sh

--- thesis/r.sh
+++ thesis/r.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 if [[ "$1" == "clean" ]]; then
-    rm -f {bachelor,master}-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
+    rm -f bachelor-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,pdf,out}
 else
     xelatex bachelor-thesis
     biber   bachelor-thesis
     xelatex bachelor-thesis
     xelatex bachelor-thesis
 
-    rm -f {bachelor,master}-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
+    rm -f bachelor-thesis.{bib,aux,log,bbl,bcf,blg,run.xml,toc,tct,out}
 fi
